9d9287bb698a13e92490b035101ea1f61d09857a
Jhonatan <jhot.256@gmail.com>
2025-02-17 17:14:08 -0500

    instalacion composer

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '3896783475195ba4f19ab153f4d6052e9b4fd582',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '3896783475195ba4f19ab153f4d6052e9b4fd582',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'phpunit/php-timer' => array(
+            'pretty_version' => '8.0.0',
+            'version' => '8.0.0.0',
+            'reference' => 'f258ce36aa457f3aa3339f9ed4c81fc66dc8c2cc',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../phpunit/php-timer',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
